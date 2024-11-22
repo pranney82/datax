6a9615c4784be81b719638867885dfa44a3dc957
ewittstruck <elliott.wittstruck@gmail.com>
2024-11-22 00:44:35 -0600

    data x

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -7,7 +7,7 @@ import {
   Send,
   Settings2,
   SquareTerminal,
-  Zap,
+  X
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
@@ -97,10 +97,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-yellow-100 text-black">
-                  <Zap className="size-4" />
+                  <X className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">Supercharged</span>
+                  <span className="truncate font-semibold">DATA x</span>
                   <span className="truncate text-xs">v0.2</span>
                 </div>
               </a>
